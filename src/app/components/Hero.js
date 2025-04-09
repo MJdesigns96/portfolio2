9f432b284c1f25291906ca1e6a5b4e7dd1e98799
Marcus Jeong <marcus.jeong96@gmail.com>
2025-04-09 13:00:55 -0400

    made changes for hero section for mobile

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -5,12 +5,12 @@ export default function Hero () {
         <div className="row my-5">
             <div className="col text-center">
                 <div className="row justify-content-center">
-                    <div className="col ms-5" data-aos="slide-right">
-                        <div className="row text-xl-start mb-5">
+                    <div className="col ms-xs-3 ms-lg-5" data-aos="slide-right">
+                        <div className="row text-xl-center mb-5">
                             <h1>Welcome!</h1>
                         </div>
-                        <div className="row text-center text-xl-start">
-                            <div className="col-8">
+                        <div className="row text-start">
+                            <div className="col">
                                 <p className='fs-4'>Hi my name is Marcus and I am a Full Stack Developer based out of Toronto Ontario, Canada.</p>
                                 <p className='fs-4'>I have worked on projects with MERN, ASP.Net, Next.Js, and Php/Laravel</p>
                                 <p className='fs-4'>If you want to see some of my work please check out my projects listed below.</p>
@@ -19,7 +19,7 @@ export default function Hero () {
                             
                         </div>
                     </div>
-                    <div className="col d-none d-xl-block" data-aos="fade-up">
+                    <div className="col d-none d-lg-block" data-aos="fade-up">
                         <img src={temp.src} className="img-fluid rounded" alt="Profile Image" style={{"height": "60vh", "borderWidth" : "2em"}}/>
                     </div>
                 </div>
